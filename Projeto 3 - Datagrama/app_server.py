6d4ae3bc64a203096847264621d49731daf10893
kadu-ymd <carlosepy@al.insper.edu.br>
2023-09-14 13:20:12 -0300

    Conceito B - Server

--- Projeto 3 - Datagrama/app_server.py
+++ Projeto 3 - Datagrama/app_server.py
@@ -4,6 +4,10 @@ import time
 import numpy as np
 
 serialName = "COM5"
+STD_HEAD = b'\xff'*12
+STD_EOP = b'\xee'*3
+MSG_NEXT = STD_HEAD + STD_EOP
+MSG_END = STD_HEAD + b'\xA0' + STD_EOP
 
 def main():
     try:
@@ -21,7 +25,7 @@ def main():
     
         # ------------------------------------------------
         # Handshake
-        handshake = b'\xff'*12 + b'\xee'*3
+        handshake = STD_HEAD + STD_EOP
         rx_handshake, n_Hs = com1.getData(15)
 
         if rx_handshake == handshake:
@@ -42,25 +46,53 @@ def main():
         rx_payload, _ = com1.getData(payload_size)
 
         _, _ = com1.getData(3)
-        image_bytes += rx_payload
-
-        print(image_bytes)
-
+        
+        cond1, cond2 = False, False
+        
         # loop
-        for i in range(1, total_pck):
-            rx_head, _ = com1.getData(12)
+        index_cond = int(rx_head[0]) # começa no 1
+        print(index_cond)
+        img_list = [rx_payload]
+        
+        com1.sendData(MSG_NEXT)
+        time.sleep(1)
 
+        for i in range(1, total_pck):
+            print('começo loop')
+            rx_head, _ = com1.getData(12)
+            
             pck_index = rx_head[0]
-            print(pck_index)
             payload_size = rx_head[1]
             total_pck = int(rx_head[2])
 
             rx_payload, _ = com1.getData(payload_size)
 
-            _, _ = com1.getData(3) # EOP
-            image_bytes += rx_payload
+            rx_eop, _ = com1.getData(3) # EOP
 
-        print(image_bytes)
+            # verifica o índice
+            if (index_cond + 1) == pck_index:
+                index_cond += 1
+                cond1 = True
+
+            # verifica se o pacote chegou completo
+            if rx_eop == (b'\xEE'*3):
+                cond2 = True
+
+            # faz um booleano das duas condições necessárias para continuar o código
+            if (cond1 and cond2):
+                img_list.append(rx_payload)
+                com1.sendData(MSG_NEXT)
+                time.sleep(1)
+            else:
+                print('Ops! Algo deu errado.')
+                
+            if pck_index == total_pck:
+                for pck in img_list:
+                    image_bytes += pck
+
+        com1.sendData(MSG_END)
+        time.sleep(3)
+
         with open(imageW, 'wb') as file:
             file.write(image_bytes)
         
