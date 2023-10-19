dfc575d68b76999e23308ae13818f9001e7ffb44
kadu-ymd <carlosepy@al.insper.edu.br>
2023-10-19 17:21:07 -0300

    server mudanças

--- projeto6_server/projeto6_server.ino
+++ projeto6_server/projeto6_server.ino
@@ -1,57 +1,65 @@
-#define RX_PIN 13  // Pino de recepção
+#define RX_PIN 3  // Pino de recepção
 #define BAUD_RATE 9600
+byte dados;  // Variável para armazenar o byte recebido
+byte bitParidade;
+byte rxParidade;
 
 void setup() {
   pinMode(RX_PIN, INPUT);
   Serial.begin(BAUD_RATE);  // Inicializa a comunicação serial
 }
 
-byte checkParidade(byte b) {
-  int cont = 0;
-  for (int i = 0; i < 8; i++) {
-    if ((b >> i) & 0x01 == 1) {
-      cont++;
-    }
-  }
-  if (cont % 2 == 0) {
-    return 0x0;
-  } else {
-    return 0x1;
+// byte checkParidade(byte b) {
+//   int cont = 0;
+//   for (int i = 0; i < 8; i++) {
+//     if ((b >> i) & 0x01 == 1) {
+//       cont++;
+//     }
+//   }
+//   if (cont % 2 == 0) {
+//     return 0x0;
+//   } else {
+//     return 0x1;
+//   }
+// }
+
+void atraso(float tempo = 1) {
+  float T = 1 / BAUD_RATE;
+  float clockT = 1 / (16 * pow(10, 6));
+  int nClocks = floor(T / clockT) + 1;
+  for (int i = 0; i < int(nClocks * tempo); i++) {
+    asm("NOP");
   }
 }
 
 void loop() {
-  byte dados;  // Variável para armazenar o byte recebido
-  byte bitParidade;
-  byte rxParidade;
+  if (digitalRead(RX_PIN) == LOW) {
+    int cont = 0;
 
-  // Aguarda o início da transmissão
-  while (digitalRead(RX_PIN) == HIGH) {
-    // Aguarda até que o nível do pino seja baixo para indicar o início da transmissão
-    // Verifica o start bit, se a leitura do pino for 1, inicia a leitura do byte
+    // while (digitalRead(RX_PIN) == HIGH) {}
+
+    atraso(1.5);
+
+    for (int i = 0; i < 8; i++) {
+      int bit = digitalRead(RX_PIN);
+      atraso();
+      dados |= (bit << i);
+      if (bit == 1) {
+        cont++;
+      }
+    }
+
+    rxParidade = digitalRead(RX_PIN);
+
+    bitParidade = cont % 2;
+
+    if (bitParidade == rxParidade) {
+      Serial.println(dados, HEX);
+      Serial.println("OK");
+    } else {
+      Serial.println(dados);
+      Serial.println("Não OK");
+    }
+    delay(2000);
   }
-
-  delayMicroseconds(1.5/BAUD_RATE);
-
-  for (int i = 0; i < 8; i++) {
-    dados |= (digitalRead(RX_PIN) << i);
-    delayMicroseconds(1/BAUD_RATE);
-  }
-  bitParidade = checkParidade(dados);
-
-  Serial.print(dados, HEX);
-  Serial.print(" ");
-
-  rxParidade = digitalRead(RX_PIN);
-  delayMicroseconds(1/BAUD_RATE);
-
-  if (rxParidade == bitParidade) {
-    Serial.print("Bit Paridade OK");
-  } else {
-    Serial.print("Bit Paridade errado");
-  }
-
-  Serial.println();
-
-
 }
