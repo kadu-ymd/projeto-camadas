fd93935e0b29fc8eb9ce45033bc6a4c216d9cd48
kadu-ymd <carlosepy@al.insper.edu.br>
2023-10-20 16:02:57 -0300

    trasmissor sem delay()

--- projeto6_server/projeto6_trasmissor/projeto6_trasmissor.ino
+++ projeto6_server/projeto6_trasmissor/projeto6_trasmissor.ino
@@ -3,6 +3,13 @@
 uint8_t dados;
 uint8_t bitParidade;
 uint8_t rxParidade;
+int nClocks = 1667;
+
+void atraso(int tempo = nClocks) {
+  for (int i = 0; i < tempo; i++) {
+    asm("NOP");
+  }
+}
 
 void setup() {
   pinMode(RX_PIN, INPUT);
@@ -15,12 +22,14 @@ void loop() {
   while (digitalRead(RX_PIN) == HIGH)
     ;
 
-  delayMicroseconds(156);
+  atraso(1.5 * nClocks);
+  // delayMicroseconds(156);
 
   for (int i = 0; i < 8; i++) {
     dados |= (digitalRead(RX_PIN) << i);
     if ((digitalRead(RX_PIN) << i) == 1) cont++;
-    delayMicroseconds(104);
+    atraso();
+    // delayMicroseconds(104);
   }
 
   rxParidade = digitalRead(RX_PIN);
