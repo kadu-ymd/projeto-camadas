5406e46823d5ce462ce11a95e6f7ae9692400869
kadu-ymd <carlosepy@al.insper.edu.br>
2023-10-27 17:55:48 -0300

    Matlab

--- Projeto 8 - Modulação AM/encode.py
+++ Projeto 8 - Modulação AM/encode.py
@@ -1,9 +1,12 @@
 from scipy.io import wavfile
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 from utils import *
+import sounddevice as sd
 
-INPUT = 'music.wav'
+INPUT = "music.wav"
+
+signal = signalMeu()
 
 sr, data = wavfile.read(INPUT)
 
@@ -11,9 +14,9 @@ SAMPLERATE = sr
 
 n_amostras = len(data)
 
-duration = n_amostras/sr # em segundos
+duration = n_amostras / sr  # em segundos
 
-t = np.linspace(0, duration, n_amostras) # em segundos
+t = np.linspace(0, duration, n_amostras)  # em segundos
 
 canal1 = [data[i][0] for i in range(n_amostras)]
 canal2 = [data[i][1] for i in range(n_amostras)]
@@ -36,4 +39,43 @@ canal2 = [data[i][1] for i in range(n_amostras)]
 
 # --------------------------------------
 
+a = 0.01134
+b = 0.0102
+c = 1
+d = -1.706
+e = 0.728
 
+y1 = [0, 0]
+
+for k in range(2, len(canal1)):
+    y1.append(-d * y1[k - 1] - e * y1[k - 2] + a * canal1[k - 1] + b * canal1[k - 2])
+
+signal.plotFFT(canal1, SAMPLERATE)
+plt.title('Sem filtro')
+
+signal.plotFFT(y1, SAMPLERATE)
+plt.title('Com filtro')
+
+# sd.play(y1, SAMPLERATE)
+# sd.wait()
+
+
+
+# --------------------------------------
+
+# Descomentar para visualizar o audio filtrado
+
+# plt.figure()
+# plt.plot(t, y1)
+# plt.plot(t, canal1)
+# plt.grid(True)
+# plt.title('Áudio do canal 1 filtrado')
+
+# plt.figure()
+# plt.plot(t, canal2)
+# plt.grid(True)
+# plt.title('Canal 2')
+
+plt.show()
+
+# --------------------------------------
