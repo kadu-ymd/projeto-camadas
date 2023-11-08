39d56b6528ee493123bdc4f84e3e9f4086baaac6
kadu-ymd <carlosepy@al.insper.edu.br>
2023-11-08 20:08:04 -0300

    Gráficos e áudios organizados para apresentação

--- Projeto 8 - Modulação AM/decode.py
+++ Projeto 8 - Modulação AM/decode.py
@@ -5,19 +5,18 @@ from utils import *
 import sounddevice as sd
 
 FREQ_C = 14000
+
+signal = signalMeu()
+
 sr, data = wavfile.read("am-music.wav")
-print(data)
+
+SAMPLERATE = sr
 
 n_amostras = len(data)
 
 duration = n_amostras / sr  # em segundos
 
 t = np.linspace(0, duration, n_amostras)  # em segundos
-
-plt.figure()
-plt.plot(t, data)
-plt.grid(True)
-plt.title('Dado na música')
 
 c_t = np.sin(2 * np.pi * FREQ_C * t)
 m_t = [data[i] * c_t[i] for i in range(len(t))]
@@ -32,9 +31,38 @@ e = 0.728
 for k in range(2, len(m_t)):
     filtrado.append(-d * filtrado[k - 1] - e * filtrado[k - 2] + a * m_t[k - 1] + b * m_t[k - 2])
 
-signalMeu().plotFFT(filtrado, sr)
+# ____________________________________________________
+
+# Áudios
+
+## Demodulado e filtrado
+
+sd.play(filtrado, SAMPLERATE)
+sd.wait()
+
+# ____________________________________________________
+
+# Gráficos
+
+plt.figure(figsize=(10, 4))
+
+plt.subplot(121)
+plt.plot(t, m_t, 'orange')
+plt.grid(True)
+plt.title("Sinal demodulado (domínio do tempo)")
+
+plt.subplot(122)
+frequency1, amplitude1 = signal.calcFFT(m_t, SAMPLERATE)
+plt.plot(frequency1, np.abs(amplitude1), 'orange')
+plt.grid(True)
+plt.title("Sinal demodulado (domínio da frequência)")
+
+plt.figure()
+frequency2, amplitude2 = signal.calcFFT(filtrado, SAMPLERATE)
+plt.plot(frequency2, np.abs(amplitude2), 'gold')
+plt.grid(True)
+plt.title("Sinal demodulado e filtrado (domínio da frequência)")
 
 plt.show()
 
-sd.play(filtrado, sr)
-sd.wait()
+# ____________________________________________________
--- Projeto 8 - Modulação AM/encode.py
+++ Projeto 8 - Modulação AM/encode.py
@@ -21,29 +21,6 @@ t = np.linspace(0, duration, n_amostras)  # em segundos
 
 canal1 = [data[i][0] for i in range(n_amostras)]
 canal1 /= (np.max(np.abs(canal1)))
-canal2 = [data[i][1] for i in range(n_amostras)]
-
-# Música original
-# sd.play(canal1, SAMPLERATE)
-# sd.wait()
-
-# --------------------------------------
-
-# Descomentar para visualizar o audio
-
-# plt.figure()
-# plt.plot(t, canal1)
-# plt.grid(True)
-# plt.title('Canal 1')
-
-# plt.figure()
-# plt.plot(t, canal2)
-# plt.grid(True)
-# plt.title('Canal 2')
-
-# plt.show()
-
-# --------------------------------------
 
 a = 0.01134
 b = 0.0102
@@ -56,41 +33,57 @@ y1 = [0, 0]
 for k in range(2, len(canal1)):
     y1.append(-d * y1[k - 1] - e * y1[k - 2] + a * canal1[k - 1] + b * canal1[k - 2])
 
-# signal.plotFFT(canal1, SAMPLERATE)
-# plt.title('Sem filtro')
-
-signal.plotFFT(y1, SAMPLERATE)
-plt.title('Com filtro')
-
 c_t = np.sin(2 * np.pi * FREQ_C * t)
 s_t = [y1[i] * c_t[i] for i in range(len(t))]
 
-signal.plotFFT(s_t, SAMPLERATE)
-plt.title('Modulado com filtro')
-
 wavfile.write("am-music.wav", SAMPLERATE, np.asarray(s_t))
 
-# Música original
+# ____________________________________________________
+
+# Áudios
+
+## Normalizado
+
+# sd.play(canal1, SAMPLERATE)
+# sd.wait()
+
+## Filtrado
+
 # sd.play(y1, SAMPLERATE)
 # sd.wait()
 
+# ____________________________________________________
 
+# Gráficos
 
-# --------------------------------------
+plt.figure()
+plt.plot(t, canal1)
+plt.grid(True)
+plt.title('Áudio normalizado')
 
-# Descomentar para visualizar o audio filtrado
+plt.figure(figsize=(8, 8))
+plt.subplot(221)
+plt.plot(t, y1, 'purple')
+plt.grid(True)
+plt.title('Áudio filtrado (domínio do tempo)')
 
-# plt.figure()
-# plt.plot(t, y1)
-# plt.plot(t, canal1)
-# plt.grid(True)
-# plt.title('Áudio do canal 1 filtrado')
+plt.subplot(222)
+frequency, amplitude = signal.calcFFT(y1, SAMPLERATE)
+plt.plot(frequency, np.abs(amplitude), 'purple')
+plt.grid(True)
+plt.title('Áudio filtrado (domínio do frequência)')
 
-# plt.figure()
-# plt.plot(t, canal2)
-# plt.grid(True)
-# plt.title('Canal 2')
+plt.subplot(223)
+plt.plot(t, s_t, 'lightgreen')
+plt.grid(True)
+plt.title('Áudio modulado (domínio do tempo)')
+
+plt.subplot(224)
+frequency, amplitude = signal.calcFFT(s_t, SAMPLERATE)
+plt.plot(frequency, np.abs(amplitude), 'lightgreen')
+plt.grid(True)
+plt.title('Áudio modulado (domínio do frequência)')
 
 plt.show()
 
-# --------------------------------------
+# ____________________________________________________
